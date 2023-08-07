7c198c46c99dae7431cf3d2d796ca32f7e9bc4da
LamNguyenLM <34997017+LamNguyenLM@users.noreply.github.com>
2023-08-07 14:57:43 +0700

    lan2

---
+++ NewApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 9;
+int b = 3;
+Console.WriteLine("{0} + {1} = {2}", a, b, a+b); 
+Console.WriteLine("Nhap vào dữ liệu chuỗi");
+string s1=Console.ReadLine();
+Console.WriteLine("NDữ liệu đã nhập là" + s1);
